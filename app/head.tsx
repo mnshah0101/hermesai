0905cc4f72949cd492b7d13b0469c698b27ae1d0
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-01-03 16:41:42 -0500

    latest

--- app/head.tsx
+++ app/head.tsx
@@ -7,6 +7,10 @@ export default function Head() {
         title="Hermes Group"
       />
       <meta name="robots" content="noindex" />
+        <meta property="og:title" content="Hermes Group" />
+        <meta property="og:description" content="Software and technology consulting focusing on artificial intelligence and next gen development for small businesses." />
+        <meta property="og:image" content="https://i.ibb.co/VjLfHPd/white-hermes-text.png" />
+        <meta property="og:url" content="https://www.hermesgroup.dev" />
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,23 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <title>Hermes Group</title>
-          <link rel="icon" type="image/x-icon" href="logos/white_hermes_.png"/>
+        <title>HermesGroup | Automation for Small Business</title>
+          <link rel="icon" type="image/x-icon" href="https://i.ibb.co/wMBW7QR/android-chrome-512x512.png"/>
 
         <meta
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1"
         />
+        <meta
+        name="description"
+        content="AI Software Solutions for Small Businesses."
+        title="Hermes Group"
+      />
+      <meta name="robots" content="noindex" />
+        <meta property="og:title" content="Hermes Group" />
+        <meta property="og:description" content="Software and technology consulting focusing on artificial intelligence and next gen development for small businesses." />
+        <meta property="og:image" content="https://i.ibb.co/FJsVKbH/black-hermes-full.png" />
+        <meta property="og:url" content="https://www.hermesgroup.dev" />
       </head>
       <body>
         <div>
